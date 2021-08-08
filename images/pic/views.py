9209bb8ad76f4f1ba0d5efdc88240a4fbe458a32
Aditya Mittal <aditmttl@gmail.com>
2021-08-08 17:27:54 +0530

    Classification API integration

--- images/pic/views.py
+++ images/pic/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from .models import Picture
 from .forms import PictureForm
-
+from .classify import classify
 from cloudinary.forms import cl_init_js_callbacks
 
 # Create your views here.
@@ -14,8 +14,16 @@ def loadPicture(request):
     if request.method == 'POST':
         form = PictureForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('index')
+            a = form.save()
+            #print('https://res.cloudinary.com/dcfcqjyxs/image/upload/v1628422595/'+a.image)
+            res = classify('https://res.cloudinary.com/dcfcqjyxs/image/upload/v1628422595/'+str(a.image))
+            print(res)
+            #print(res['class'],res['class_probability'])
+            context ={
+                'type' : res,
+                'image' : 'https://res.cloudinary.com/dcfcqjyxs/image/upload/v1628422595/'+str(a.image),
+            }
+            return render(request, 'pic/index.html',context=context)
             
     form = PictureForm()
     ctx = { 'form': form }
