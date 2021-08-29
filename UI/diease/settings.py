807c56a38a4d5a8ff363c805b6dc676085f2db26
Aditya Mittal <76921082+adityamittl@users.noreply.github.com>
2021-08-29 20:33:44 +0530

    Update settings.py

--- UI/diease/settings.py
+++ UI/diease/settings.py
@@ -155,6 +155,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'diagnosedelta1@gmail.com'
-EMAIL_HOST_PASSWORD = 'delta@123'
+EMAIL_HOST_PASSWORD = 'mnhibtaunga'
 
 LOGIN_URL = 'auth/login'
